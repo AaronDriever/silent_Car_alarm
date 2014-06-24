a15f6f9140945e4f8584f4a6070e808b5544e946
AaronDriever <aarondriever@gmail.com>
2014-06-23 18:33:33 -0700

    adsf

--- Silent_Car_Alarm/Silent_Car_Alarm.ino
+++ Silent_Car_Alarm/Silent_Car_Alarm.ino
@@ -1,13 +1,58 @@
+/*
+Silent car alarm Tx end using an exbee and a arduino Fio
+*/
+#include <XBee.h>
+
+
+#define triggerPin 9
+
+
+//SoftwareSerial xbee(2, 3); // RX, TX xbee pins
+
+
+int triggerState;
+
+long previousMillis = 0; 
+long currentMillis = 100;
+long interval = 500;
+
 void setup()
 {
-
-  /* add setup code here */
-
+	//Serial.begin(9600);
+	Serial1.begin(9600);
+	pinMode(triggerPin, INPUT);	// declare pin 9 to be an output:
 }
 
 void loop()
 {
+	armed();
+}
 
-  /* add main program code here */
+void test()
+{
+	Serial1.println("hello"); //xbee
+}
 
+void armed()
+{
+	currentMillis = millis(); 
+	if((currentMillis - previousMillis) > interval)
+	{  
+		previousMillis += interval; // save the last time you sent data
+		Serial1.println(triggerState);
+	} 
+}
+
+void test1()
+{
+	triggerState = digitalRead(triggerPin);
+	if (triggerState > 0)
+	{  
+		Serial1.println(triggerState);
+	} 
+
+	else
+	{
+		test();
+	}
 }
--- Silent_Car_Alarm/Visual Micro/.Silent_Car_Alarm.vsarduino.h
+++ Silent_Car_Alarm/Visual Micro/.Silent_Car_Alarm.vsarduino.h
@@ -5,18 +5,19 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Uno, Platform=avr, Package=arduino
+	Hardware: SparkFun Fio V3 3.3V/8MHz, Platform=avr, Package=SF32u4_boards-master
 */
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega328p__
-#define __AVR_ATmega328P__
+#define __AVR_ATmega32u4__
+#define __AVR_ATmega32U4__
+#define _VMDEBUG 1
 #define ARDUINO 105
 #define ARDUINO_MAIN
 #define __AVR__
 #define __avr__
-#define F_CPU 16000000L
+#define F_CPU 8000000L
 #define __cplusplus
 #define __inline__
 #define __asm__(x)
@@ -41,8 +42,11 @@ extern "C" void __cxa_pure_virtual() {;}
 
 //
 //
+void test();
+void armed();
+void test1();
 
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
-#include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\standard\pins_arduino.h" 
-#include "F:\gDrive\Sketches and libraries\Silent_Car_Alarm\Silent_Car_Alarm.ino"
+#include "C:\Program Files (x86)\Arduino\hardware\SF32u4_boards-master\variants\promicro\pins_arduino.h" 
+#include "F:\MyRepo\silent_Car_alarm\Silent_Car_Alarm\Silent_Car_Alarm.ino"
 #endif
